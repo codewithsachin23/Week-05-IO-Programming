a0df352a4cd0dcbb72938e568f21a593cf472811
sachin shukla <sachinshukla232003@gmail.com>
2025-02-19 10:20:22 +0530

    [Sachin Shukla]Add.Detect Duplicate

---
+++ src/main/java/com/day01/ioprogramming/advancedproblems/detectduplicatesCSVfile/DetectDuplicatesInACSVFile.java
@@ -0,0 +1,9 @@
+package com.day01.ioprogramming.advancedproblems.detectduplicatesCSVfile;
+import static com.day01.ioprogramming.advancedproblems.detectduplicatesCSVfile.DetectingDuplicatesInACSVFile.detectDuplicates;
+
+public class DetectDuplicatesInACSVFile {
+    public static void main(String[] args) {
+        String filePath = "readData.csv";
+        detectDuplicates(filePath);
+    }
+}
